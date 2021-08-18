b70cda4cd442a27c80e9c5eb52109aeef8f049a4
ReactionaryFuture <ABanzhof@gmail.com>
2021-08-17 23:44:12 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -12,22 +14,27 @@ func main() {
 	app := app.New()
 	window := app.NewWindow("math_practice")
 
-	data := binding.NewFloat()
-	data.Set(30)
-	entry := widget.NewEntryWithData(binding.FloatToStringWithFormat(data, "%v"))
-	slide := widget.NewSliderWithData(1, 60, data)
+	addCard := mkNewCheckCard("Addition")
+	subCard := mkNewCheckCard("Subtraction")
+	mltCard := mkNewCheckCard("Multiplication")
+	checkCards := container.NewGridWithColumns(3, addCard, subCard, mltCard)
 
-	cont := container.NewBorder(nil, nil, nil, entry, slide)
+	numOfProblems := mkNewSliderCard(10, 50, 999, "How many problems?")
+	timerSlider := mkNewSliderCard(1, 20, 999, "Seconds per problem?")
+	reShuffleSlider := mkNewSliderCard(1, 10, 99, "Reshuffle times?")
+	sliderCards := container.NewGridWithColumns(3, numOfProblems, timerSlider, reShuffleSlider)
+
+	cont := container.NewVBox(checkCards, sliderCards)
 
 	window.SetContent(cont)
 	window.Resize(fyne.NewSize(300, 1))
 	window.ShowAndRun()
 }
 
-func makeCheckboxCard(mathType string) (*widget.Card, *[]*widget.Check) {
-	// this function makes a card for a math type (i.e. + - *) with check
-	// boxes that allow the inclusion or exclusion of various tables (i.e.
-	// multiplication tables)
+func mkNewCheckCard(mathType string) *widget.Card {
+	// this function makes a widget.Card for a math type (i.e. + - *)
+	// with check boxes that allow the inclusion or exclusion of various
+	// tables (i.e. multiplication tables)
 
 	// make check boxes for number sets 1 through 12 and set them to checked
 	var chkBxs []*widget.Check
@@ -52,27 +59,59 @@ func makeCheckboxCard(mathType string) (*widget.Card, *[]*widget.Check) {
 	})
 
 	// put both buttons in a container with horizontal layout
-	btnCont := container.NewGridWithColumns(
-		2, checkAllBoxesBtn, uncheckAllBoxesBtn)
+	btnCont := container.NewGridWithColumns(2, (checkAllBoxesBtn), (uncheckAllBoxesBtn))
 
 	// put all check boxes in a container in a grid pattern
-	vBox1 := container.NewGridWithColumns(
+	row1 := container.NewGridWithColumns(
 		4, chkBxs[1], chkBxs[2], chkBxs[3], chkBxs[4])
-	vBox2 := container.NewGridWithColumns(
+	row2 := container.NewGridWithColumns(
 		4, chkBxs[5], chkBxs[6], chkBxs[7], chkBxs[8])
-	vBox3 := container.NewGridWithColumns(
+	row3 := container.NewGridWithColumns(
 		4, chkBxs[9], chkBxs[10], chkBxs[11], chkBxs[12])
-	checkBoxCont := container.NewGridWithRows(3, vBox1, vBox2, vBox3)
+	chkBxGrid := container.NewGridWithRows(3, row1, row2, row3)
 
 	// put the button container and the check box container together in a
 	// vertical layout
-	finalLayout := container.NewVBox(btnCont, checkBoxCont)
+	finalLayout := container.NewVBox(btnCont, chkBxGrid)
 
 	// wrap finalLayout in a card
 	card := widget.NewCard(mathType, "", finalLayout)
 
 	// return the card and the state map
-	return card, &chkBxs
+	return card
 }
 
-func makeSliderWithEntry()
+func mkNewSliderCard(sliderMin, sliderMax, maxVal int, label string) *widget.Card {
+	// this function returns a widget.Card with slider and entry widgets
+	// that are bound together
+
+	// make slider and slider widgets that are bound toether
+	data := binding.NewFloat()
+	data.Set(float64((sliderMin + sliderMax) / 2))
+	entry := widget.NewEntryWithData(binding.FloatToStringWithFormat(data, "%v"))
+	slide := widget.NewSliderWithData(float64(sliderMin), float64(sliderMax), data)
+
+	// check entry for soundness. set to half max for entries that can
+	// not be easily converted to ints and set to maxValue for entries
+	// greater than the max value
+	entry.OnChanged = func(s string) {
+		i, err := strconv.Atoi(s)
+		if s == "" {
+			return
+		} else if err != nil {
+			data.Set(float64((sliderMin + sliderMax) / 2))
+		} else if i > maxVal {
+			data.Set(float64(maxVal))
+		} else {
+			data.Set(float64(i))
+		}
+	}
+
+	// make a container with layout for the widgets
+	cont := container.NewBorder(nil, nil, nil, entry, slide)
+
+	// wrap container in card
+	card := widget.NewCard("", label, cont)
+
+	return card
+}
