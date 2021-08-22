1aafaff9b6352054e80bc92ce5bff91b570c4227
ReactionaryFuture <ABanzhof@gmail.com>
2021-08-22 18:17:22 -0400

    what is the point of this?

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"github/ReactionaryFuture/math_practice/data"
 	"github/ReactionaryFuture/math_practice/ui"
+
+	"fyne.io/fyne/v2/app"
 )
 
 func main() {
-	fmt.Println(data.DataTest())
-	fmt.Println(ui.UiTest())
+	app := app.New()
+	window := app.NewWindow("math_practice")
+
+	optionsScreen := ui.NewOptionsScreen()
+
+	window.SetContent(optionsScreen.CanvasObject)
+	window.ShowAndRun()
 }
